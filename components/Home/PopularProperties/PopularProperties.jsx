b9c2787330d4342cd033c5863b427f0d5c3df5d5
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-09 13:31:02 +0300

    Add properties data and images

--- components/Home/PopularProperties/PopularProperties.jsx
+++ components/Home/PopularProperties/PopularProperties.jsx
@@ -1,7 +1,19 @@
 import styles from './popularProperties.module.scss';
+import PropertyCard from '@/components/Common/PropertyCard/PropertyCard';
+// import properties from '../../../'
 
 function PopularProperties() {
-  return <section className={styles.container}></section>;
+  // const recentProperties = properties
+  // ?.sort(() => Math.random() - Math.random())
+  // .slice(0, 3);
+  return (
+    <section className={styles.container}>
+      <h2>Popular Properties</h2>
+      <div className={styles.cards}>
+        {Array.from([1, 2, 3].map((card, i) => <PropertyCard key={i} />))}
+      </div>
+    </section>
+  );
 }
 
 export default PopularProperties;
