d618fde416e89283ff314bb20f848fcd026a0e01
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-09 21:13:58 +0300

    Add button to view all properties

--- components/Home/RecentProperties/RecentProperties.jsx
+++ components/Home/RecentProperties/RecentProperties.jsx
@@ -2,21 +2,29 @@ import styles from './recentProperties.module.scss';
 import PropertyCard from '@/components/Common/PropertyCard/PropertyCard';
 import properties from '@/properties.json';
 import { inter } from '@/public/font/font';
+import Link from 'next/link';
 
 function RecentProperties() {
   const recentProperties = properties.slice(0, 3);
 
   return (
-    <section className={`${styles.container} ${inter.className}`}>
-      <h2>
-        Recent <span>Properties</span>
-      </h2>
-      <div className={styles.cards}>
-        {recentProperties.map((property, i) => (
-          <PropertyCard key={i} property={property} />
-        ))}
-      </div>
-    </section>
+    <>
+      <section className={`${styles.container} ${inter.className}`}>
+        <h2>
+          Recent <span>Properties</span>
+        </h2>
+        <div className={styles.cards}>
+          {recentProperties.map((property, i) => (
+            <PropertyCard key={i} property={property} />
+          ))}
+        </div>
+      </section>
+      <section className={styles.bigBtn}>
+        <Link className={`btn ${styles.btn}`} href="/properties">
+          View all properties
+        </Link>
+      </section>
+    </>
   );
 }
 
