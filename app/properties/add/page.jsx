2cdbb096570226d5150b7e9cd3949866a6acf83e
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-12 13:38:40 +0300

    Add footer to addProperty page

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,10 +1,12 @@
-import AddProperty from '@/components/AddProperty/AddProperty';
 import { inter } from '@/public/font/font';
+import AddProperty from '@/components/AddProperty/AddProperty';
+import Footer from '@/components/Common/Footer/Footer';
 
 function AddPropertyPage() {
   return (
     <div className={inter.className}>
       <AddProperty />
+      <Footer />
     </div>
   );
 }
