6510699e454204936045b8ee4d971211e8310445
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-14 19:08:14 +0300

    Add profile and notifications avatars

--- components/Common/Header/Navbar/Navbar.jsx
+++ components/Common/Header/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 'use client';
 import styles from './navbar.module.scss';
-import { AiOutlineMessage } from 'react-icons/ai';
-import { IoMdPerson, IoIosMenu } from 'react-icons/io';
 import Logo from './Logo';
+import { IoIosMenu } from 'react-icons/io';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import profileDefault from '@/public/images/profile.png';
 
 function Navbar({ toggleSidebar }) {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
   return (
     <div className={styles.nav}>
       <div onClick={toggleSidebar} className={`${styles.burger}`}>
@@ -14,8 +18,38 @@ function Navbar({ toggleSidebar }) {
       <Logo />
 
       <div className={styles.right}>
-        <AiOutlineMessage />
-        <IoMdPerson />
+        <Link href="/messages" className={styles.messages}>
+          <button type="button" className={styles.btn}>
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+              />
+            </svg>
+          </button>
+          {/* <UnreadMessagesCount session={session} /> */}
+        </Link>
+
+        <div className={styles.profile}>
+          <div className={styles.avatar}>
+            <button
+              type="button"
+              className={styles.btn}
+              aria-expanded="false"
+              aria-haspopup="true"
+              //   onClick={() => setIsProfileOpen((prev) => !prev)}
+            >
+              <Image src={profileDefault} fill alt="profile avatar" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
