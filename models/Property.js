8f394540624e61ef274e5060f792816bd0bc9e83
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-26 08:21:05 +0300

    feat: Add Property schema

---
+++ models/Property.js
@@ -0,0 +1,90 @@
+import { Schema, model, models } from 'mongoose';
+
+const PropertySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      county: {
+        type: String,
+        required: true,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: {
+      type: [String],
+    },
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    is_popular: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Property = models.Property || model('Property', PropertySchema);
+
+export default Property;
