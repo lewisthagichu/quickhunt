4f512bd0209b48326b0be026aa39480e1acb2b60
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-27 17:51:15 +0300

    feat: Add useCallbackUrl hook

---
+++ hooks/useCallbackUrl.jsx
@@ -0,0 +1,10 @@
+'use client';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+export default function useCallbackUrl() {
+  const pathname = usePathname();
+  const params = useSearchParams().toString();
+  // if there are no params, don't add the ?
+  const callbackUrl = `${pathname}${params ? '?' : ''}${params}`;
+  return callbackUrl;
+}
