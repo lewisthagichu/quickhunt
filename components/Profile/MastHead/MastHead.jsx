2210098a47131a98a62c6bf42d36ed3bc4fc2b49
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-28 16:20:51 +0300

    refactor: Add dynamic data to MastHead component

--- components/Profile/MastHead/MastHead.jsx
+++ components/Profile/MastHead/MastHead.jsx
@@ -1,17 +1,37 @@
-import Link from 'next/link';
+'use client';
 import styles from './mastHead.module.scss';
 import { inter } from '@/public/font/font';
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-function MastHead() {
+import profileDefault from '@/public/images/profile.png';
+
+export default function MastHead() {
+  const { data: session } = useSession();
+  const { name, image, createdAt } = session?.user;
+  const date = new Date(createdAt).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+  });
+
+  useEffect(() => {}, [session]);
+
   return (
     <section className={`${styles.container} ${inter.className}`}>
       <div className={styles.profile}>
         <div className={styles.avatar}>
-          <span>L</span>
+          <Image
+            src={image || profileDefault}
+            fill
+            sizes="50vw"
+            alt="profile photo"
+          />
         </div>
         <div className={styles.content}>
-          <h1>Lewis</h1>
-          <p>Member since Aug 2024</p>
+          <h1>{name}</h1>
+          <p>Member since {date}</p>
         </div>
       </div>
 
@@ -32,5 +52,3 @@ function MastHead() {
     </section>
   );
 }
-
-export default MastHead;
