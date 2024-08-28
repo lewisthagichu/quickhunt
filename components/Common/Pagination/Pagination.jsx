becfeb5739fb5746a6459fac843a8358d0a7299a
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-28 11:30:27 +0300

    feat: get properties page data from the database

--- components/Common/Pagination/Pagination.jsx
+++ components/Common/Pagination/Pagination.jsx
@@ -1,11 +1,29 @@
+'use client';
 import styles from './pagination.module.scss';
 
-function Pagination() {
+function Pagination({ page, pageSize, total, onPageChange }) {
+  const totalPages = Math.ceil(total / pageSize);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <button>Previous</button>
-      <span>1 of 1</span>
-      <button>Next</button>
+      <button disabled={page === 1} onClick={() => handlePageChange(page - 1)}>
+        Previous
+      </button>
+      <span>
+        {page} of {totalPages}
+      </span>
+      <button
+        disabled={page === totalPages}
+        onClick={() => handlePageChange(page + 1)}
+      >
+        Next
+      </button>
     </div>
   );
 }
--- components/Properties/Wrapper/PropertiesWrapper.jsx
+++ components/Properties/Wrapper/PropertiesWrapper.jsx
@@ -1,16 +1,55 @@
+'use client';
 import styles from './wrapper.module.scss';
+import { useState, useEffect } from 'react';
 import Pagination from '@/components/Common/Pagination/Pagination';
 import PropertyCard from '@/components/Common/PropertyCard/PropertyCard';
 import PlaceholderCards from '@/components/Common/PlaceholderCards/PlaceholderCards';
-import properties from '@/test.json';
+import Spinner from '@/components/Spinner';
 
 function PropertiesWrapper() {
-  // const properties = [];
-  const listings = properties.slice(0, 3);
-  const hasListings = listings.length > 0;
-  return (
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(6);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch(
+          `/api/properties?page=${page}&pageSize=${pageSize}`
+        );
+
+        if (res.status !== 200) {
+          throw new Error();
+        }
+
+        const { total, properties } = await res.json();
+        setProperties(properties);
+        setTotal(total);
+      } catch (error) {
+        // Handle errors
+        console.log(error);
+        toast.error('Failed to fetch properties');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProperties();
+  }, [page, pageSize]);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
+
+  const hasProperties = properties?.length > 0;
+
+  return loading ? (
+    <Spinner />
+  ) : (
     <section className={styles.container}>
-      {hasListings ? (
+      {hasProperties ? (
         <div className={styles.cards}>
           {properties.map((property, index) => (
             <div key={property.id || `l_${index}`} className={styles.item}>
@@ -26,7 +65,15 @@ function PropertiesWrapper() {
           linkText="Back to Home"
         />
       )}
-      <Pagination />
+      {hasProperties && (
+        <Pagination
+          page={page}
+          pageSize={pageSize}
+          total={total}
+          setPage={setPage}
+          onPageChange={handlePageChange}
+        />
+      )}
     </section>
   );
 }
