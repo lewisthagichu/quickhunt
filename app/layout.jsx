c6f1ca5dabcc4ecae4e794070f9c55e591c0be6a
lewisthagichu <lewisthagichu254@gmail.com>
2024-08-31 10:23:17 +0300

    chore: Install react-loading-skeleton

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import 'photoswipe/dist/photoswipe.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { ToastContainer } from 'react-toastify';
 import { GlobalContextProvider } from '@/context/GlobalContext';
 import getServerUser from '@/lib/getSessionUser';
